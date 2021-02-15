40cfa77d2670b5f83f59e7ca7d015edc809d4e0d
jobla <jobla@DESKTOP-09CV834.home>
2021-02-15 14:49:52 -0500

    Prints out even numbers from 0 to 100

---
+++ EVENNUMBERSJB/src/evennumbersjb/EVENNUMBERSJB.java
@@ -0,0 +1,12 @@
+package evennumbersjb;
+public class EVENNUMBERSJB {
+
+    public static void main(String[] args) {
+       for(int i = 0; i <=100; i+=2 )
+           //prints out even numbers from 0 to 100
+          System.out.println(i);
+       //prints on a new line
+       
+    }
+    
+}
